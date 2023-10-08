2613146ddede3fe36573ee512eac85ad3e54a6db
HewkawAr <earthwangsaeng2@gmail.com>
2023-10-09 01:28:11 +0700

    1.0-SNAPSHOT

---
+++ src/main/java/hewkawar/plugin/M2SMP.java
@@ -0,0 +1,17 @@
+package hewkawar.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class M2SMP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
